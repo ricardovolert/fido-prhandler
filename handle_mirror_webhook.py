ff2c1f9aa9b6cd30b8f1be761c165125a18ee5bf
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-05-01 11:03:22 -0500

    update mirror bot to mirror to bitbucket

--- handle_mirror_webhook.py
+++ handle_mirror_webhook.py
@@ -8,23 +8,21 @@ from tornado.web import RequestHandler, Application
 
 LOCAL_GIT_REPO_PATH = os.environ.get('LOCAL_GIT_REPO_PATH', '/tmp/yt-git')
 LOCAL_HG_REPO_PATH = os.environ.get('LOCAL_HG_REPO_PATH', '/tmp/yt-hg')
-HG_REPO = os.environ.get('HG_REPO', 'https://bitbucket.org/yt_analysis/yt')
-GH_REPO = os.environ.get(
-    'GH_REPO', 'git+ssh://git@github.com/yt-project/yt.git')
-GIT_BRANCH_MAP = {
-    'yt': 'master',
-    'stable': 'stable',
-    'yt-2.x': 'yt-2.x',
-}
-
-
-def get_revision_from_remote_repo(repo, name):
-    head = repo.identify(id=True, rev='remote(%s, default)' % name)
-    return head.decode().strip()
+HG_REPO = os.environ.get('HG_REPO', 'ssh://hg@bitbucket.org/yt_analysis/yt')
+GH_REPO = os.environ.get('GH_REPO', 'git@github.com:yt-project/yt.git')
 
 
 @gen.coroutine
 def sync_repos():
+    try:
+        gh_repo = git.Repo(LOCAL_GIT_REPO_PATH)
+    except (git.exc.NoSuchPathError, git.exc.InvalidGitRepositoryError):
+        print('initializing %s' % LOCAL_GIT_REPO_PATH)
+        gh_repo = git.Repo.init(LOCAL_GIT_REPO_PATH)
+        gh_repo.create_remote('origin', GH_REPO)
+    finally:
+        gh_repo.close()
+
     configs = ['extensions.hggit=']
     try:
         repo = hglib.open(LOCAL_HG_REPO_PATH, configs=configs)
@@ -32,39 +30,26 @@ def sync_repos():
     except hglib.error.ServerError:
         print('cloning %s to %s' % (HG_REPO, LOCAL_HG_REPO_PATH))
         hglib.clone(source=HG_REPO, dest=LOCAL_HG_REPO_PATH)
-
-    try:
-        gh_repo = git.Repo(LOCAL_GIT_REPO_PATH)
-    except (git.exc.NoSuchPathError, git.exc.InvalidGitRepositoryError):
-        print('initializing %s' % LOCAL_GIT_REPO_PATH)
-        gh_repo = git.Repo.init(LOCAL_GIT_REPO_PATH, bare=True)
-        gh_repo.create_remote('origin', GH_REPO)
-    finally:
-        gh_repo.close()
-
-    with hglib.open(LOCAL_HG_REPO_PATH, configs=configs) as repo:
-        repo.pull(HG_REPO)
-        for hg_branch in ['yt', 'stable', 'yt-2.x']:
-            rev = get_revision_from_remote_repo(repo, hg_branch)
-            print('updating to %s on branch %s' % (rev, hg_branch))
-            repo.update(rev, check=True)
-            git_branch = GIT_BRANCH_MAP[hg_branch]
-            print('setting bookmark %s on branch %s' % (git_branch, hg_branch))
-            repo.bookmark(git_branch, force=True)
-            print('pushing to %s on branch %s' % (
-                LOCAL_GIT_REPO_PATH, git_branch))
-            repo.push(LOCAL_GIT_REPO_PATH, bookmark=git_branch)
-        # ensure tags are pushed
-        print('pushing to %s' % LOCAL_GIT_REPO_PATH)
-        repo.push(LOCAL_GIT_REPO_PATH)
+        # need to do this to ensure the correct hashes in the
+        # converted git repo since two-way conversion is lossy see
+        # e.g. https://groups.google.com/forum/#!topic/hg-git/1r1LBrqLeXc
+        with hglib.open(LOCAL_HG_REPO_PATH, configs=configs) as repo:
+            print('pushing %s to %s' % 
+                  (LOCAL_HG_REPO_PATH, LOCAL_GIT_REPO_PATH))
+            repo.push(LOCAL_GIT_REPO_PATH)
 
     with git.Repo(LOCAL_GIT_REPO_PATH) as repo:
-        for git_branch in GIT_BRANCH_MAP.values():
-            print('pushing to %s from %s on branch %s' % (
-                GH_REPO, LOCAL_GIT_REPO_PATH, git_branch))
-            repo.remotes.origin.push(git_branch)
-        print('pushing tags to %s from %s' % (GH_REPO, LOCAL_GIT_REPO_PATH))
-        repo.remotes.origin.push(tags=True)
+        print('pull from %s on branch master' % GH_REPO)
+        repo.remotes.origin.pull('master')
+
+    with hglib.open(LOCAL_HG_REPO_PATH, configs=configs) as repo:
+        print('pull from %s to %s on branch master' % 
+              (LOCAL_GIT_REPO_PATH, LOCAL_HG_REPO_PATH))
+        repo.pull(LOCAL_GIT_REPO_PATH)
+        repo.update('master', check=True)
+        print('push from %s to %s on bookmark master' %
+              (LOCAL_HG_REPO_PATH, HG_REPO))
+        repo.push(HG_REPO, bookmark='master')
     print('Done!')
 
 
